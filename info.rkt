339ac3b08f17d75219a09a06dc88af06fdc59cb9
Alex Owens <aowens5189@gmail.com>
2020-01-07 19:16:22 -0500

    Removing broken dependency 'core'

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define version "0.1")
-(define deps (list "beautiful-racket" "brag" "draw-lib" "gui-lib" "core"))
+(define deps (list "beautiful-racket" "brag" "draw-lib" "gui-lib"))
 (define test-omit-paths (list "examples" "expander.rkt"))
