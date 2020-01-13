793508158edb324bd8cc371e89a5c5ea5c3736d2
Alex Owens <aowens5189@gmail.com>
2020-01-13 05:57:00 -0500

    Explicitly specifying base dependency

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define version "0.1")
-(define deps (list "beautiful-racket" "brag" "draw-lib" "gui-lib"))
+(define deps (list "beautiful-racket" "brag" "draw-lib" "gui-lib" "base"))
 (define test-omit-paths (list "examples" "expander.rkt"))
