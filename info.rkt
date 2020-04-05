546b0ae5ba0b5c9c36b4d7585ee579d733732cc4
Alex Owens <aowens@MacBook-Pro-4.local>
2020-04-05 17:49:01 -0400

    Starting on Puzzler documentation, wrote up Sokoban example. Also moved paper to directory called 'background' and updated info.rkt with documentation.

--- info.rkt
+++ info.rkt
@@ -2,3 +2,4 @@
 (define version "0.1")
 (define deps (list "beautiful-racket" "brag" "draw-lib" "gui-lib" "base" "parser-tools-lib"))
 (define test-omit-paths (list "examples" "expander.rkt"))
+(define scribblings '(("scribblings/puzzler.scrbl" ())))
